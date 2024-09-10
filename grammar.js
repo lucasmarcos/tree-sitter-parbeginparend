a863081a0e87ab2e38eb9b2bf45dea2936edec15
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2024-09-10 18:44:31 -0300

    init

---
+++ grammar.js
@@ -0,0 +1,18 @@
+module.exports = grammar({
+  name: "parbeginparend",
+  extras: $ => [
+    /\s/,
+    $.comment
+  ],
+  rules: {
+    source_file: $ => repeat($._statement),
+    _statement: $ => choice($.begin, $.end, $.parbegin, $.parend, $.call),
+    begin: $ => "BEGIN",
+    end: $ => "END",
+    parbegin: $ => "PARBEGIN",
+    parend: $ => "PAREND",
+    label: $ => /[a-zA-Z_][a-zA-Z_0-9]*/,
+    call: $ => seq($.label, ";"),
+    comment: $ => seq("//", /.*/)
+  }
+});
